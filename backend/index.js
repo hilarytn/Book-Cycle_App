a7bb021d97af737f2916d756ed27a79752bbdd78
Hilary Titus Naor <hilarytn@gmail.com>
2023-09-09 14:50:43 +0100

    Define first route

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import userRoutes from './routes/user.js';
+//const express = require('express');
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+app.use('/user', userRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Home Page, Welcome!');
+})
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
