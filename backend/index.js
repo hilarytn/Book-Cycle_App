cbe591505556c0479376584af5aaeccfa02f00bd
Hilary Titus Naor <hilarytn@gmail.com>
2023-09-10 16:13:13 +0100

    Create GET/POST  users endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import userRoutes from './routes/user.js';
+import users from './routes/miniDB.js';
 //const express = require('express');
 
 const app = express();
@@ -11,4 +12,11 @@ app.use('/user', userRoutes);
 app.get('/', (req, res) => {
     res.send('Home Page, Welcome!');
 })
+
+app.get('/users', (req, res) => {
+    console.log(users);
+    res.json(users);
+})
+
+
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import users from './miniDB.js';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
 router.get('/', (req, res) => res.send("Coming soon!"));
+
+router.post('/', (req, res) => {
+    const user = req.body;
+    users.push({ ...user, id: uuidv4(),
+        createdAt: new Date(),
+        updatedAt: new Date()});
+    res.send(`User with the name ${user.name} has been successfully created!`)
+    console.log(`User with the name ${user.name} has been successfully created!`);
+});
 
 export default router;
