8964e771d2f9aa1177ae2a56c337f62bb17671e0
Hilary Titus Naor <hilarytn@gmail.com>
2023-09-13 19:16:40 +0100

    Rephrase editUserDetails response

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,29 +10,8 @@ export const getUserById = async (req, res) => {
     res.status(200).json({ message: 'User fetched!' });
 }
 
-export const editUserDetails = (req, res) => {
-    const { id } = req.params;
-    const {
-        name,
-        username,
-        gender,
-        dob,
-        address,
-        email,
-        telephone,
-    } = req.body;
-
-    const user = users.find((user) => user.id == id);
-    if (name) user.name = name;
-    if (username) user.username = username;
-    if (gender) user.gender = gender;
-    if (dob) user.dob = dob;
-    if (address) user.address = address;
-    if (email) user.email = email;
-    if (telephone) user.telephone = telephone;
-
-    res.send(`Success, User ${id} updated!`);
-    console.log(`Success, User ${id} updated!`);
+export const editUserDetails = async (req, res) => {
+    res.status(200).json({ message: 'User details updated!' });
 }
 
 export const deleteUser = (req, res) => {
