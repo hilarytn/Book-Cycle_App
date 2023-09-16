e304153c965f0024643066fcbd1c420f27207f35
ripeplantain <irvingmanny@gmail.com>
2023-09-16 01:49:59 +0000

    fully responsive

--- client/src/components/Home/Books.tsx
+++ client/src/components/Home/Books.tsx
@@ -13,7 +13,7 @@ const Books: React.FC<Props> = ({setMoreBooks}) => {
     <>
         <section className="md:p-12">
             <div className='flex justify-between items-center my-10 px-6 md:ps-20'>
-                <h1 className='text-lg md:text-2xl text-black font-bold tracking-wider capitalize'>
+                <h1 className='text-xl md:text-2xl text-black font-bold tracking-wider capitalize'>
                     Recommended for you
                 </h1>
                 <p 
@@ -23,31 +23,27 @@ const Books: React.FC<Props> = ({setMoreBooks}) => {
                 </p>
             </div>
             <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-10">
-                {dummyBooks.map((book, index) => (
-                    <div key={index} className='flex flex-col justify-center items-center gap-4'>
-                        <img
-                            className='w-[200px] h-[300px] object-cover rounded-md'
-                            src={book.coverUrl} 
-                            alt="book cover" />
-                        <p className='text-black text-[18px] tracking-wider'>
-                            {book.title}
-                        </p>
-                        <p className='text-secondary text-[18px] tracking-wider'>
-                            {book.author}
-                        </p>
-                        <div>
-                            <button className='bg-secondary text-white px-4 py-2 rounded-md tracking-wider hover:bg-gray-800 hover:scale-75 delay-100 uppercase'>
-                                Chat with owner
-                            </button>
-                        </div>
+            {dummyBooks.map((book, index) => (
+                <div key={index} className="max-w-xs mx-auto bg-white shadow-lg rounded-lg overflow-hidden m-4">
+                    <img className="w-full h-40 object-cover" src={book.coverUrl} alt="Book Cover" />
+                    <div className="p-4">
+                      <h2 className="text-xl font-bold mb-2">{book.title}</h2>
+                      <p className="text-gray-600 text-sm mb-2">{book.author}</p>
+                      <p className="text-gray-800 text-base">{book.description}</p>
+                      <div className="mt-4 flex gap-3">
+                        <button className="px-4 py-2 bg-secondary text-white rounded hover:bg-green-700">Chat Owner</button>
+                      </div>
                     </div>
-                ))}
+                </div>
+            ))}
             </div>
         </section>
 
+        <hr className="mt-6" />
+
         <section className="md:p-12">
             <div className='flex justify-between items-center my-10 px-6 md:ps-20'>
-                <h1 className='text-lg md:text-2xl text-black font-bold tracking-wider capitalize'>
+                <h1 className='text-xl md:text-2xl text-black font-bold tracking-wider capitalize'>
                     Latest books
                 </h1>
                 <p 
@@ -58,21 +54,15 @@ const Books: React.FC<Props> = ({setMoreBooks}) => {
             </div>
             <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-10">
                 {dummyBooks.map((book, index) => (
-                    <div key={index} className='flex flex-col justify-center items-center gap-4'>
-                        <img
-                            className='w-[200px] h-[300px] object-cover rounded-md'
-                            src={book.coverUrl} 
-                            alt="book cover" />
-                        <p className='text-black text-[18px] tracking-wider'>
-                            {book.title}
-                        </p>
-                        <p className='text-secondary text-[18px] tracking-wider'>
-                            {book.author}
-                        </p>
-                        <div>
-                            <button className='bg-secondary text-white px-4 py-2 rounded-md tracking-wider hover:bg-gray-800 hover:scale-75 delay-100 uppercase'>
-                                Chat with owner
-                            </button>
+                    <div key={index} className="max-w-xs mx-auto bg-white shadow-lg rounded-lg overflow-hidden m-4">
+                        <img className="w-full h-40 object-cover" src={book.coverUrl} alt="Book Cover" />
+                        <div className="p-4">
+                          <h2 className="text-xl font-bold mb-2">{book.title}</h2>
+                          <p className="text-gray-600 text-sm mb-2">{book.author}</p>
+                          <p className="text-gray-800 text-base">{book.description}</p>
+                          <div className="mt-4 flex gap-3">
+                            <button className="px-4 py-2 bg-secondary text-white rounded hover:bg-green-700">Chat Owner</button>
+                          </div>
                         </div>
                     </div>
                 ))}
--- client/src/pages/App/HomePage.tsx
+++ client/src/pages/App/HomePage.tsx
@@ -16,15 +16,15 @@ const HomePage = () => {
                     bg-hero-pattern bg-center bg-cover bg-blend-multiply bg-black/[.50]'
         >
           <div className='flex flex-col justify-center items-center gap-8'>
-            <h1 className='text-[60px] text-white font-bold tracking-wider capitalize'>
+            <h1 className='text-3xl md:text-[50px] text-white font-bold tracking-wider capitalize'>
               Welcome to Book Cycle
             </h1>
-            <p className='text-white text-[18px] tracking-wider'>
+            <p className='text-white text-sm md:text-[18px] tracking-wider'>
               Fostering Friendships through the Love of Books
             </p>
             <form className='flex gap-3'>
                 <input
-                      className='bg-gray-200 px-4 py-4 rounded-md tracking-wider' 
+                      className='bg-gray-200 py-4 rounded-md tracking-wider' 
                       type="search" />
                 <button className='bg-secondary text-white px-4 py-2 rounded-md tracking-wider hover:bg-gray-800 hover:scale-75 delay-100 uppercase'>
                   Search
