fb656dbf90644a938ea75b94fa773365d82b822b
sethdanny <naddulidaniel1994@gmail.com>
2023-09-16 03:48:14 +0300

    fixed

---
+++ backend/controllers/authController.js
@@ -0,0 +1,35 @@
+import User from "../models/User.js";
+//import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+
+// @desc    Register a new user
+// @route   POST /api/auth/register
+// @access  Public
+export const registerUser = async (req, res) => {
+    try {
+        const { username, email, password, firstName, lastName, phoneNumber, address } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const newUser = await User.create({
+            username,
+            email,
+            password: hashedPassword,
+            firstName,
+            lastName,
+            phoneNumber,
+            address
+        });
+        await newUser.save();
+        res.status(201).json({ message: "User created successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server Error" });
+    }
+};
+
+// @desc    Login a user
+// @route   POST /api/auth/login
+// @access  Public
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
-import userRoutes from './routes/user.js';
 import bookRoutes from './routes/books.js';
 import connectDB from './config/db.js';
+import authRoutes from './routes/auth.js';
 
-// Load environment variables from the .env file into process.env
+
 dotenv.config();
 
 connectDB();
@@ -14,11 +14,13 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
-app.use('/api', userRoutes);
+
+app.use('/api', authRoutes);
 app.use('/api', bookRoutes);
 
+
 app.get('/', (req, res) => {
-  res.send('Home Page, Welcome!');
+  res.send('Home Page, Welcome! to Book-Cycle App - API');
 });
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
